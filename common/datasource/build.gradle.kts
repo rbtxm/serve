27faf4cbdd83fa540265e0ac15a02326b9f75591
guanxiangkai <xiangkaiguan@iCloud.com>
2023-04-28 11:39:14 +0800

    放弃使用springdatajpa，使用mybatisplus

--- common/datasource/build.gradle.kts
+++ common/datasource/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies{
-    api("org.springframework.boot:spring-boot-starter-data-jpa")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation(project(":common:core"))
+    implementation(libs.dynamic.datasource.spring.boot.starter)
 }
