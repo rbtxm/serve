f26f9c7b568c8f7b604302924d4f85173a9aa35a
guanxiangkai <xiangkaiguan@iCloud.com>
2023-05-31 19:07:38 +0800

    更新

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,59 +11,22 @@ pluginManagement {
     }
 }
 rootProject.name = "serve"
-//
-//include("gateway")
-//
-//include("auth")
-//
-//include("common")
-//include("common:core","common:datasource","common:redis","common:doc","common:security","common:log","common:datascope","common:seata","common:sentinel")
-//
-//include("module")
-//include("module:system","module:file")
-//
-//include("api")
-//include("api:system","api:file")
 
-val projectDirs = listOf("gateway", "auth", "common", "module")
-
-val commonProjectDirs = listOf("redis", "doc", "security", "log", "datascope", "seata", "sentinel")
-
-projectDirs("", projectDirs)
-
-projectDirs("common", commonProjectDirs)
-
-
-fun projectDirs(path: String, projectDirs: List<String>) {
-    val dir: File = if (path.isEmpty()) {
-        rootDir
-    } else {
-        File(rootDir, path)
-    }
-    projectDirs.forEach { dirName ->
-        val subdir = File(dir, dirName)
-        subdir.walkTopDown().maxDepth(1).forEach { dir ->
-            val buildFile = File(dir, "${dir.name}.gradle.kts")
-            if (buildFile.exists()) {
-                includeProject(path, dirName, dir.name)
-            }
-        }
+rootDir.walkTopDown().maxDepth(2).forEach { dir ->
+    println(dir.name)
+    val buildFile = File(dir, "${dir.name}.gradle.kts")
+    if (buildFile.exists()) {
+        includeProject(dir.name)
     }
 }
 
-fun includeProject(path: String, projectDirName: String, projectName: String) {
-    val baseDir = File(settingsDir, projectDirName)
-    val projectDir: File = if (path.isEmpty()) {
-        File(baseDir, projectName)
-    } else {
-        File(File(baseDir, path), projectName)
-    }
+fun includeProject(projectName: String) {
+    val baseDir = File(settingsDir, projectName)
+    val projectDir = File(baseDir, projectName)
     val buildFileName = "${projectName}.gradle.kts"
     assert(projectDir.isDirectory)
     assert(File(projectDir, buildFileName).isFile)
-
     include(projectName)
-
     project(":${projectName}").projectDir = projectDir
     project(":${projectName}").buildFileName = buildFileName
 }
