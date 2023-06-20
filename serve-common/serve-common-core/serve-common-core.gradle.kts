9c2c3080a9c98d1c7d974bdbfb588b4346d50a74
guanxiangkai <xiangkaiguan@iCloud.com>
2023-06-20 18:58:58 +0800

    优化

--- serve-common/serve-common-core/serve-common-core.gradle.kts
+++ serve-common/serve-common-core/serve-common-core.gradle.kts
@@ -1,14 +1,5 @@
 dependencies {
-    implementation(libs.guava.get().toString())
     implementation(libs.hutool.core.get().toString())
-    implementation(libs.poi.ooxml.get().toString())
     implementation(libs.commons.lang3.get().toString())
     implementation(libs.commons.io.get().toString())
-    implementation(libs.commons.fileupload.get().toString())
-    implementation(libs.transmittable.thread.local.get().toString())
-    implementation(libs.spring.boot.starter.validation.get().toString())
-    implementation(libs.spring.boot.starter.web.get().toString()){
-        exclude(libs.spring.boot.starter.tomcat.get().toString())
-    }
-    implementation(libs.spring.boot.starter.undertow.get().toString())
 }
